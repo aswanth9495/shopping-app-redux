540484a3aa3346001ea3d676442bd49a5bf05486
aswanth9495 <aswanth@interviewbit.com>
2021-12-04 22:36:18 +0530

    Some commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import cartReducer from './cart';
+import postsReducer from './posts';
 
 const reducer = combineReducers({
   cartStore : cartReducer,
+  postsStore: postsReducer
 });
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
 
---
+++ src/store/posts.js
@@ -0,0 +1,31 @@
+const GET_POSTS = 'posts/GET_POST';
+
+export const getPosts = () => {
+  return async (dispatch, getState) => {
+    console.log(getState(), 'State values');
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    response = await response.json();
+    if (response) {
+      dispatch({ type: GET_POSTS, payload: response })
+    }
+  }
+}
+
+export default function reducer(
+  state = {
+    posts: []
+  },
+  action,
+) {
+  switch(action.type) {
+    case GET_POSTS: {
+      // We'll store the posts inside the post state here
+      return {
+        ...state,
+        posts: action.payload
+      }
+    }
+    default:
+      return state;
+  }
+}
